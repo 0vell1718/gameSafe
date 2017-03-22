e8e7ff2df2440e67b0fdef771f30f1949c33099a
0vell1718 <0vell1718@gmail.com>
2017-03-23 01:55:24 +0300

    read N remove basic controls draw Levers(buttons)

--- gameSafe/gameSafe/Form1.Designer.cs
+++ gameSafe/gameSafe/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.infoBtn = new System.Windows.Forms.Button();
+            this.infoTbx = new System.Windows.Forms.TextBox();
+            this.infoLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // infoBtn
+            // 
+            this.infoBtn.Location = new System.Drawing.Point(196, 157);
+            this.infoBtn.Name = "infoBtn";
+            this.infoBtn.Size = new System.Drawing.Size(111, 35);
+            this.infoBtn.TabIndex = 0;
+            this.infoBtn.Text = "button1";
+            this.infoBtn.UseVisualStyleBackColor = true;
+            this.infoBtn.Click += new System.EventHandler(this.infoBtn_Click);
+            // 
+            // infoTbx
+            // 
+            this.infoTbx.Location = new System.Drawing.Point(226, 131);
+            this.infoTbx.Name = "infoTbx";
+            this.infoTbx.Size = new System.Drawing.Size(53, 20);
+            this.infoTbx.TabIndex = 1;
+            // 
+            // infoLbl
+            // 
+            this.infoLbl.AutoSize = true;
+            this.infoLbl.Location = new System.Drawing.Point(175, 106);
+            this.infoLbl.Name = "infoLbl";
+            this.infoLbl.Size = new System.Drawing.Size(152, 13);
+            this.infoLbl.TabIndex = 2;
+            this.infoLbl.Text = "Введите размер сейфа NxN:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 363);
+            this.Controls.Add(this.infoLbl);
+            this.Controls.Add(this.infoTbx);
+            this.Controls.Add(this.infoBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button infoBtn;
+        private System.Windows.Forms.TextBox infoTbx;
+        private System.Windows.Forms.Label infoLbl;
     }
 }
 
--- gameSafe/gameSafe/Form1.cs
+++ gameSafe/gameSafe/Form1.cs
@@ -12,9 +12,69 @@ namespace gameSafe
 {
     public partial class Form1 : Form
     {
+        List<Lever> levers;
+        int N;
+        Random rnd = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            levers = new List<Lever>();
         }
+
+        public void AddLever()
+        {
+            int r = rnd.Next(1, 3);
+            Position pos = Position.Horizontal;
+
+            if (r == 2)
+                pos = Position.Vertical;
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new Size(50, 10);
+                    btn.Location = new Point(50 * i, 50 * j);
+                    Controls.Add(btn);
+                    levers.Add(new Lever(btn, pos));
+                }
+            }
+        }
+
+        private void infoBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                N = Convert.ToInt32(infoTbx.Text);
+                Controls.Remove(infoTbx);
+                Controls.Remove(infoLbl);
+                Controls.Remove(infoBtn);
+                AddLever();
+            }
+            catch
+            {
+                MessageBox.Show("Введите целое значение!");
+            }          
+        }
+    }
+
+    public class Lever
+    {
+        Button btn;
+        Position pos;
+
+        public Lever(Button btn, Position pos)
+        {
+            this.btn = btn;
+            this.pos = pos;
+        }
+    }
+
+    public enum Position
+    {
+        Horizontal,
+        Vertical
     }
 }
